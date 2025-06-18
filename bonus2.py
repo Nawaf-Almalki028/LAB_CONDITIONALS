ed983f3705045a78bbbfd72cb79ca562e889df18
nawaf-almalki028 <xp.mo91@gmail.com>
2025-06-18 12:55:17 +0300

    movie,bonus1,bonus2 done! adding name or mail message

--- bonus2.py
+++ bonus2.py
@@ -6,4 +6,13 @@ x = len(Name)
 if x > 2 and Email.endswith("@gmail.com"):
     print(f"welcome {Name}, you registered with the email {Email} !")
 else:
-    print("the email is not valid , please provide a valid email .")
+    if not Email.endswith("@gmail.com") and x <= 2:
+
+        print("the email is not valid , please provide a valid email .")
+        print("the name length must be more than 2 characters, please provide a valid name.")
+
+    elif not Email.endswith("@gmail.com"):
+        print("the email is not valid , please provide a valid email .")
+    elif x <= 2:
+        print("the name length must be more than 2 characters, please provide a valid name.")
+
